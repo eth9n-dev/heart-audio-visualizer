93b4c9e33e6da862eeab878f2c479642776d5517
eth9n-dev <eckert6@gmail.com>
2024-01-17 11:55:18 -0800

    commenting code

--- visualizer.js
+++ visualizer.js
@@ -1,46 +1,46 @@
 var song;
 var fft;
 var particles = [];
-var positions = [];
-var message = 'I LOVE YOU SOPHIA';
+var positions = []; // particle origin positions
+var message = 'I LOVE YOU SOPHIA'; // message default
 
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function preload() {
+function preload() { // preload song
     song = loadSound('imgod.mp3');
 }
 
-function setup() {
+function setup() { // waveform analysis + canvas creation
     createCanvas(windowWidth, windowHeight);
     fft = new p5.FFT();
 }
 
-function windowResized() { 
+function windowResized() { // resize canvas if window size is changed
     resizeCanvas(windowWidth, windowHeight); 
 }
 
 function keyPressed() {
-    if (keyCode === 49) {
+    if (keyCode === 49) { // 1
         song.jump(130);
     }
-    else if (keyCode === 50) {
+    else if (keyCode === 50) { // 2
         song.jump(339);
     }
-    else if (keyCode === 51) {
+    else if (keyCode === 51) { // 3
         song.jump(579);
     }
-    else if (keyCode === 52) {
+    else if (keyCode === 52) { // 4
         song.jump(742);
     }
-    else if (keyCode === 53) {
+    else if (keyCode === 53) { // 5
         song.jump(973);
     }
-    else if (keyCode === 54) {
+    else if (keyCode === 54) { // 6
         song.jump(1184);
     }
-    else if (keyCode === 48) {
+    else if (keyCode === 48) { // 0
         song.jump(0);
     }
 }
@@ -54,7 +54,7 @@ function draw() {
     fft.analyze();
     amp = fft.getEnergy(20, 200);
 
-    push()
+    push() // draw text + shake animation
     if (amp > 100) {
         rotate(random(-0.03, 0.03));
         textSize(26);
@@ -68,8 +68,8 @@ function draw() {
 
     var wave = fft.waveform();
 
-    beginShape();
-    drawingContext.shadowBlur = 32;
+    beginShape(); // draw heart
+    drawingContext.shadowBlur = 32; // add neon effect
     drawingContext.shadowColor = color(255, 0, 0);
     for (var i = 0; i < 25; i += .1) {
         var index = floor(map(i, 0, width, 0, wave.length));
@@ -84,34 +84,32 @@ function draw() {
     }
     endShape();
 
-    if (amp > 100) {
+    if (amp > 100) { // if amp is high enough, create particles
         var randomPos = positions[randomIntFromInterval(0, 250)];
         var p = new Particle(randomPos);
         particles.push(p);
     }
     
 
-    for (var i = particles.length - 1; i >= 0; i--) {
-        if (!particles[i].edges()) {
-            particles[i].update(amp > 150);
+    for (var i = particles.length - 1; i >= 0; i--) { // iterate through our particles
+        if (!particles[i].edges()) { // if not off screen
+            particles[i].update(amp > 150); // add velocity if amp is high
             particles[i].show();
         }
         else {
-            particles.splice(i, 1);
+            particles.splice(i, 1); // remove particle
         }
     }
 
-    if (song.currentTime() >= song.duration() - 6) {
+    if (song.currentTime() >= song.duration() - 6) { // change text before song ends
         message = 'WILL YOU BE MY \nVALENTINE?';
     }
     else {
         message = 'I LOVE YOU SOPHIA';
     }
-
-    print(song.currentTime(), song.duration());
 }
 
-function mouseClicked() {
+function mouseClicked() { // play/pause functionality
     if (song.isPlaying()) {
         song.pause();
     }
@@ -123,10 +121,10 @@ function mouseClicked() {
 class Particle {
     
     constructor(vec) {
-        this.pos = createVector(vec.x, vec.y);
-        this.vel = createVector(0, 0);
-        this.acc = this.pos.copy().mult(random(0.0001, 0.00001));
-        this.w = random(3, 5);
+        this.pos = createVector(vec.x, vec.y); // position
+        this.vel = createVector(0, 0); // velocity
+        this.acc = this.pos.copy().mult(random(0.0001, 0.00001)); // acceleration
+        this.w = random(3, 5); // width
     }
     
     update(cond) {
@@ -140,7 +138,7 @@ class Particle {
         }
     }
 
-    edges() {
+    edges() { // edge of canvas check
         if (this.pos.x < -width / 2 || this.pos.x > width / 2 ||
         this.pos.y < -height / 2 || this.pos.y > width / 2) {
             return true;
